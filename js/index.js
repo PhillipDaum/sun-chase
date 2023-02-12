1c870b2c80d994a83654838e5bb167255c8848a4
Phil Daum <philgdaum@gmail.com>
2023-02-11 22:44:34 -0800

    trying to figure out JS

---
+++ js/index.js
@@ -0,0 +1,17 @@
+
+function sunshineUS() {
+  
+  for (var k = 0; k < cities.length; k++) {
+    //if the one with the number length you are on
+    for (cities.length[k] = ) {      
+    if (Country = 'United States') {
+            let printer = document.createElement('div')
+            console.log([k].City);
+            printer.innerHTML = cities[k].City;
+            printer.className = "text-warning fs-3 ms-3 mb-2 font-monospace lh-sm";
+            document.getElementById("USsunshine").appendChild(printer)
+
+          }
+        }
+      }
+}
