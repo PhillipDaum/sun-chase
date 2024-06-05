055028aca688841a5a76b8bdc3abe38ff7283732
Phil <philgdaum@gmail.com>
2024-06-04 20:49:22 -0600

    working on bug- line 20 index.js

--- js/index.js
+++ js/index.js
@@ -1,8 +1,8 @@
 const form = document.getElementById("form");
 let userArray = [];
+let result = []; 
 
 form.addEventListener("submit", onFormSubmit);
-
 function onFormSubmit(event) {
 	event.preventDefault();
 	const data = new FormData(event.target);
@@ -12,7 +12,31 @@ function onFormSubmit(event) {
 	userArray.push(dataObject);
 	console.log(userArray);
 	form.reset();
+
+  let sunMonth = userArray[0].month;
+  console.log(typeof sunMonth, sunMonth);
+  for (let i = 0; i < cities.length; i++) {
+    // why is this undefined?
+    console.log(cities[i].sunMonth);
+    console.log(sunMonth);
+  }
+
 }
+
+
+
+
+// first lets produce all of the results with the same continent
+// for loop - count through all of these
+// if the cities[i].continent === userArray[0].continent
+// then push that into result
+// variable =  is prolly just gonna be cities[i]. - the whole thing??? 
+// result.push(variable) -- it does one of these each loop
+
+
+// another variable will be let sunmonth = userArray.month
+
+
 
 
 // function to show what cities are likely to have sun right now
